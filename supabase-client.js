0b6bb091a3098ab51ba0de9a55abe07079bad39b
OTAKU-1234 <garnolhensromulus12@gmail.com>
2026-08-26 13:01:55 -0400

    Rename SUPABASE_PUBLISHABLE_KEY to SUPABASE_KEY

--- supabase-client.js
+++ supabase-client.js
@@ -1,8 +1,10 @@
 const SUPABASE_URL = "https://dwgcsxwzeebmwatilmso.supabase.co";
 
-const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_0PKxrhbq6mUrRdQEldc3LQ_kn6qnXjV";
+const SUPABASE_KEY = "sb_publishable_0PKxrhbq6mUrRdQEldc3LQ_kn6qnXjV";
 
-const supabaseClient = window.supabase.createClient(
+const supabaseClient = supabase.createClient(
     SUPABASE_URL,
-    SUPABASE_PUBLISHABLE_KEY
+    SUPABASE_KEY
 );
+
+window.supabaseClient = supabaseClient;
